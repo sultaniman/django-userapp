d5c759fe668aab95aa27a5e4a22f2950a17e09cf
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2015-03-04 16:08:29 +0600

    Added backend authenticate code

--- django_userapp/backends.py
+++ django_userapp/backends.py
@@ -1,5 +1,34 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth import get_user_model
+from .request import login
+
+import re
+
+
+UserModel = get_user_model()
 
 
 class UserappBackend(object):
-    pass
+    def authenticate(self, username=None, password=None, request=None, **kwargs):
+        result = login(request, username=username, password=password)
+
+        try:
+            if result is None:
+                raise UserModel.DoesNotExist("Userapp account not found")
+
+            user = result[0]
+            our_username = re.sub("@\-", "_", username)
+            our_user, created = UserModel.objects.get_or_create(username=our_username,
+                                                                email=user["email"])
+
+            return our_user
+        except UserModel.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return UserModel._default_manager.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
